6563ecf640849c4ffcb895032fe7391438649858
xiaohuli <waterdoor@foxmail.com>
2020-12-25 14:23:34 +0800

    fix: 日拱一卒

---
+++ 手撸代码系列/call&bind&apply.js
@@ -0,0 +1,46 @@
+Function.prototype.myCall = function(incontext) {
+    if (typeof this !== 'function') {
+      throw '请在函数上调用'
+    }
+    const context = incontext || window;
+    const fn = this;
+    context.fn = fn;
+    const args = [...arguments].slice(1);
+    let res = context.fn(...args);
+    delete context.fn;
+    return res;
+  }
+
+  Function.prototype.myApply = function(incontext) {
+    if (typeof this !== 'function') {
+      throw '请在函数上调用'
+    }
+    const context = incontext || window;
+    const fn = this;
+    context.fn = fn;
+    const args = [...arguments].slice(1);
+    let res;
+    if (arguments[1]) {
+      res = context.fn(arguments[1]);
+    } else {
+      res = context.fn()
+    }
+    delete context.fn;
+    return res;
+  }
+
+  Function.prototype.myBind = function(incontext) {
+    if (typeof this !== 'function') {
+      throw '请在函数上调用'
+    }
+    const context = incontext || window;
+    const _this = this;
+    const outerArg = [...arguments].slice(1);
+    return function F() {
+      if (_this instanceof F) {
+        //  因为bing只有第一次生效，所以要判断，如果上次是bind，那就不要再绑定context了
+        return _this(arguments.contact(outerArg));
+      }
+      return _this.apply(incontext, arguments.contact(outerArg))
+    }
+  }
