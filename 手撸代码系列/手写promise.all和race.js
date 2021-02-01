0e8923565b0517c469182bb1fd62923166c28c7c
xiaohuli <waterdoor@foxmail.com>
2021-02-01 13:41:59 +0800

    fix: 补足

--- 手撸代码系列/手写promise.all和race.js
+++ 手撸代码系列/手写promise.all和race.js
@@ -1,18 +1,32 @@
-const myAll = function(arr) {
-    let results = new Array(arr.length).fill(false);
-    if (!arr.every(item => item && (typeof item === 'object' || typeof item === 'function'))) {
-      throw Error('比如都是promise');
-    }
+//  这个实现有点问题，不能单纯根据值来判断
+// const myAll = function(arr) {
+//     let results = new Array(arr.length).fill(false);
+//     if (!arr.every(item => item && (typeof item === 'object' || typeof item === 'function'))) {
+//       throw Error('比如都是promise');
+//     }
+//     return new Promise((res, rej) => {
+//       arr.forEach((item, index) => {
+//         item.then(_ => {
+//           results[index] = _;
+//           if (results.every(item => item)) {
+//             res(results);
+//           }
+//         }, e => {
+//           rej(e)
+//         })
+//       })
+//     })
+//   }
+  function myAll(arr) {
     return new Promise((res, rej) => {
+      let result = new Array(res.length).fill({ isNotFinish: true });
       arr.forEach((item, index) => {
-        item.then(_ => {
-          results[index] = _;
-          if (results.every(item => item)) {
-            res(results);
+        item.then(value => {
+          result[index] = Object.assign({}, result[index], { value, isNotFinish: false });
+          if(res.length === arr.length && res.every(item => !item.isNotFinish)) {
+            res(result.map(item => item.value))
           }
-        }, e => {
-          rej(e)
-        })
+        }, e => rej(e))
       })
     })
   }
